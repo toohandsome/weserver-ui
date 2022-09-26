d069f2fc2d26ebb773d28601ce05186407902c9d
faq <hudcan@sina.com>
2022-09-26 23:19:24 +0800

    文件页完善

--- src/components/FileTop/index.vue
+++ src/components/FileTop/index.vue
@@ -2,53 +2,71 @@
 
   <div style="display:flex;width: 100%;">
 
-    <div style="width: calc(80% - 10px);margin-right: 10px;">
+    <div style="width: 70%;margin-right: 10px;">
 
-      <!-- 后退 -->
+      <div style="display:flex;width: 100%;">
+        <div style="width: 76px;">
+          <!-- 导航 -->
+          <el-button-group>
+            <!-- 后退 -->
+            <el-button :icon="caretLeft" @click="leftClick" @mouseenter="leftBtnMouseenter"
+              @mouseleave="leftBtnMouseleave" />
+            <!-- 前进 -->
+            <el-button :icon="caretRight" @click="rightClick" @mouseenter="rightBtnMouseenter"
+              @mouseleave="rightBtnMouseleave" />
+            <!-- 向上 -->
+            <el-button :icon="caretTop" @click="topClick" @mouseenter="topBtnMouseenter"
+              @mouseleave="topBtnMouseleave" />
+          </el-button-group>
+        </div>
+        <div style="width: calc(100% - 76px);">
 
-      <el-button :icon="caretLeft" @click="backClick" @mouseenter="backMouseenter" @mouseleave="backMouseout"
-        :type="backType" />
+          <!-- 路径输入框 -->
+          <el-input v-model="inputPath" @focus="pathInputFocus" @blur="pathInputBlue">
 
+            <!-- 路径按钮 -->
+            <template #prefix>
+              <div v-for="(path, i) in pathArr" :key="i" class="pathButGroup" v-show="showPathBut">
+                <el-button @click="backClick" width="100px">
+                  {{path}}
+                </el-button>
+                <el-button @click="backClick" width="5px" :icon="arrowRight">
+                </el-button>
+              </div>
+            </template>
 
-      <!-- 前进 -->
+            <!-- 刷新按钮 -->
+            <template #append>
+              <el-button :icon="refresh" />
+            </template>
 
-      <el-button :icon="caretRigth" @click="backClick" @mouseenter="backMouseenter" @mouseleave="backMouseout"
-        :type="backType" />
-
-
-      <!-- 向上 -->
-
-      <el-button :icon="caretTop" @click="backClick" @mouseenter="backMouseenter" @mouseleave="backMouseout"
-        :type="backType" />
-
-      <!-- 路径输入框 -->
-      <el-input v-model="inputPath" @focus="pathInputFocus" @blur="pathInputBlue">
-
-        <!-- 路径按钮 -->
-        <template>
-          <div v-for="(path, i) in pathArr" :key="i" class="pathButGroup" v-show="showPathBut">
-            <el-button @click="backClick" width="100px">
-              {{path}}
-            </el-button>
-            <el-button @click="backClick" width="5px" :icon="arrowRight">
-            </el-button>
-          </div>
-        </template>
-
-        <!-- 刷新按钮 -->
-        <template #append>
-          <el-button :icon="refresh" />
-        </template>
-
-      </el-input>
-
+          </el-input>
+        </div>
+      </div>
     </div>
-    <div style="width: 20%">
+
+    <div style="width: 20% ;display:flex;">
       <!-- 搜索框 -->
       <el-input v-model="searchPath">
+
+        <template #append>
+          <!-- 搜索按钮 -->
+          <el-dropdown>
+            <el-button :icon="search">
+              <el-icon class="el-icon--right">
+                <arrow-down />
+              </el-icon>
+            </el-button>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>高级</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+
+        </template>
       </el-input>
-      <!-- 搜索按钮 -->
-      <el-button :icon="search" />
+
 
     </div>
 
@@ -58,7 +76,7 @@
 
 <script>
 
-import { ArrowRight, CaretLeft, Refresh } from '@element-plus/icons-vue';
+import { ArrowRight, CaretLeft, CaretRight, CaretTop, Refresh, Search } from '@element-plus/icons-vue';
 
 export default {
 
@@ -71,6 +89,9 @@ export default {
       arrowRight: ArrowRight,
       caretLeft: CaretLeft,
       refresh: Refresh,
+      caretRight: CaretRight,
+      caretTop: CaretTop,
+      search: Search,
       searchPath: ""
     }
   },
@@ -83,17 +104,62 @@ export default {
       this.showPathBut = true;
       console.log("pathInputBlue", this.showPathBut);
     },
-    backClick() {
-      console.log("back");
+    leftClick() {
+      console.log("leftClick");
+    }, rightClick() {
+      console.log("rightClick");
+    }, topClick() {
+      console.log("topClick");
     },
-    backMouseenter() {
+    leftBtnMouseenter() {
       console.log("鼠标划入");
       this.backType = "primary"
     },
-    backMouseout() {
+    leftBtnMouseleave() {
+      console.log("按钮松开");
+      this.backType = "default"
+    },
+    rightBtnMouseenter() {
+      console.log("鼠标划入");
+      this.backType = "primary"
+    },
+    rightBtnMouseleave() {
+      console.log("按钮松开");
+      this.backType = "default"
+    },
+    topBtnMouseenter() {
+      console.log("鼠标划入");
+      this.backType = "primary"
+    },
+    topBtnMouseleave() {
       console.log("按钮松开");
       this.backType = "default"
     }
   }
 }
 </script>
+
+<style lang='scss' scoped>
+.el-button-group .el-button {
+  padding: 8px 5px
+}
+
+.pathButGroup .el-button {
+  margin-left: 0px;
+  border-radius: 0px;
+  padding: 2px;
+  border: 0.5px;
+  background-color: #f5f7fa;
+  margin-top: 1.5px;
+  height: 29.5px;
+  // border: 0px;
+}
+
+:deep .el-input__prefix {
+  transform: translateX(-11px) !important;
+}
+
+:deep .el-input__inner {
+  padding-left: 6px !important;
+}
+</style>
--- src/views/serverManage/file/index.vue
+++ src/views/serverManage/file/index.vue
@@ -1,14 +1,90 @@
 <template>
    <div class="app-container">
-       <FileTop></FileTop>
+      <FileTop></FileTop>
+      <el-divider />
+      <div style="display:flex;width: 100%;">
+         <div style="width: 70%;margin-bottom: 5px;    margin-top: -10px;">
+            <el-button> 上传 </el-button>
+            <el-dropdown>
+               <el-button :icon="search">
+                  新建
+                  <el-icon class="el-icon--right">
+                     <arrow-down />
+                  </el-icon>
+               </el-button>
+               <template #dropdown>
+                  <el-dropdown-menu>
+                     <el-dropdown-item>文件</el-dropdown-item>
+                     <el-dropdown-item>文件夹</el-dropdown-item>
+                  </el-dropdown-menu>
+               </template>
+            </el-dropdown>
+            <el-button> 终端 </el-button>
+         </div>
+         <div style="width: 30%; ">
+            <el-button-group>
+               <el-button type="primary" :icon="ArrowLeft">图标模式</el-button>
+               <el-button type="primary">
+                  列表模式  
+               </el-button>
+            </el-button-group>
+         </div>
+      </div>
+      <el-table ref="multipleTableRef" :data="tableData" style="width: 100%" @selection-change="handleSelectionChange">
+         <el-table-column type="selection" width="55" />
+         <el-table-column property="fileName" label="文件名" show-overflow-tooltip>
+         </el-table-column>
+         <el-table-column property="onwer" label="所有者/权限" width="150" />
+         <el-table-column property="size" label="大小" width="150" />
+         <el-table-column property="upTime" label="修改时间" width="240" />
+         <el-table-column label="操作" align="right">
+
+            <template #default="scope">
+               <el-button-group>
+                  <el-button size="small" @click="handleEdit(scope.$index, scope.row)">打开</el-button>
+
+                  <el-dropdown>
+                     <el-button :icon="search" size="small">
+                        复制
+                        <el-icon class="el-icon--right">
+                           <arrow-down />
+                        </el-icon>
+                     </el-button>
+                     <template #dropdown>
+                        <el-dropdown-menu>
+                           <el-dropdown-item>剪切</el-dropdown-item>
+                           <el-dropdown-item>重命名</el-dropdown-item>
+                           <el-dropdown-item type="danger">删除</el-dropdown-item>
+                           <el-dropdown-item type="danger">压缩</el-dropdown-item>
+                        </el-dropdown-menu>
+                     </template>
+                  </el-dropdown>
+
+
+                  <el-button size="small" @click="handleEdit(scope.$index, scope.row)">下载</el-button>
+
+                  <el-button size="small" @click="handleEdit(scope.$index, scope.row)">属性</el-button>
+
+               </el-button-group>
+            </template>
+
+         </el-table-column>
+
+      </el-table>
+
+       <div class="block">
+         <el-pagination v-model:currentPage="currentPage4" v-model:page-size="pageSize4"
+            :page-sizes="[100, 200, 300, 400]" :small="small" :disabled="disabled" background
+            layout="total, sizes, prev, pager, next, jumper" :total="400" @size-change="handleSizeChange"
+            @current-change="handleCurrentChange" />
+      </div>
+
    </div>
 </template>
  
 
 <script>
-import { list as initData } from "@/api/monitor/online";
 import { ArrowRight, CaretLeft, Refresh } from '@element-plus/icons-vue';
-import FileTop from '@/components/FileTop'
 export default {
    data() {
       return {
@@ -19,8 +95,27 @@ export default {
          arrowRight: ArrowRight,
          caretLeft: CaretLeft,
          refresh: Refresh,
-         searchPath:"",
-         
+         searchPath: "",
+         tableData: [
+            {
+               "fileName": "1.txt",
+               "onwer": "755/root",
+               "size": "9.65 KB",
+               "upTime": "2020-01-01 09:23:22"
+            },
+            {
+               "fileName": "YXD-LOG.txt",
+               "onwer": "777/root",
+               "size": "109 MB",
+               "upTime": "2022-11-11 23:46:52"
+            },
+            {
+               "fileName": "wps.exe",
+               "onwer": "755/root",
+               "size": "145 MB",
+               "upTime": "2022-05-08 19:32:27"
+            }
+         ]
       }
    },
    methods: {
@@ -48,25 +143,25 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.pathButGroup .el-button {
-   margin-left: 0px;
-   border-radius: 0px;
-   padding: 2px;
-   border: 0.5px;
-   background-color: #f5f7fa;
-   margin-top: 1.5px;
-   height: 29.5px;
-   // border: 0px;
-}
+// .pathButGroup .el-button {
+//    margin-left: 0px;
+//    border-radius: 0px;
+//    padding: 2px;
+//    border: 0.5px;
+//    background-color: #f5f7fa;
+//    margin-top: 1.5px;
+//    height: 29.5px;
+//    // border: 0px;
+// }
 
 
-:deep .el-input__prefix {
-   transform: translateX(35px) !important;
-}
+// :deep .el-input__prefix {
+//    transform: translateX(35px) !important;
+// }
 
-:deep .el-input__inner {
-   padding-left: 6px !important;
-}
+// :deep .el-input__inner {
+//    padding-left: 6px !important;
+// }
 
 
 // .input-with-select .el-input-group__prepend {
